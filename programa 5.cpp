37b0262149a2645be6ff5d6a6c1868f1b1fb482d
ESCOBAR SANDOVAL OSVALDO VALENTIN <168395096+VALENCITOOOO@users.noreply.github.com>
2024-06-06 12:26:54 -0600

    Add files via upload

---
+++ programa 5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;  //si las horas es mayor a 40, salario aumenta 1.5
+int main(){
+
+    double horas, precio_hora, salario;
+	cout<<"ingrese las horas trabajadas: ";
+	cin>>horas;
+	cout<<"ingrese el pago por hora: ";
+	cin>>precio_hora;
+    for ()
+
+	salario= horas * precio_hora;
+
+	cout<<"el salario a pagar es: " << salario << endl;
+	return 0;
+}
