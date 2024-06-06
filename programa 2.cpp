94f866c596a5de6af886af4d187103ea4e14bd23
ESCOBAR SANDOVAL OSVALDO VALENTIN <168395096+VALENCITOOOO@users.noreply.github.com>
2024-06-06 11:45:59 -0600

    Add files via upload

---
+++ programa 2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector> //utilizo vectores
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+  // Declaración de variables
+  vector<int> numeros(5); // Vector para almacenar los 5 números
+  int menor, mayor;
+  bool hayRepetidos = false;
+
+  // para ingresar los 5 numeros
+  
+  cout << "Ingrese 5 números: " << endl;
+  for (int i = 0; i < 5; i++) {
+    cin >> numeros[i];
+  }
+
+  menor = mayor = numeros[0];
+  for (int i = 1; i < 5; i++) {
+    if (numeros[i] < menor) {
+      menor = numeros[i];
+    } else if (numeros[i] > mayor) {
+      mayor = numeros[i];
+    }
+  }
+
+
+  sort(numeros.begin(), numeros.end()); 
+  for (int i = 1; i < 5; i++) {
+    if (numeros[i] == numeros[i - 1]) {
+      hayRepetidos = true;
+      cout << "Número repetido " << numeros[i] << endl; 
+    }
+  }
+
+  // decir cual es menor y mayor
+  cout << "El número menor es " << menor << endl;
+  cout << "El número mayor es " << mayor << endl;
+
+  return 0;
+}
