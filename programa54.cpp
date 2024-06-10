a371d3e78a99a49f4964b2e1300c6a792c966c2b
ESCOBAR SANDOVAL OSVALDO VALENTIN <168395096+VALENCITOOOO@users.noreply.github.com>
2024-06-10 10:45:20 -0600

    Create programa54.cpp

---
+++ programa54.cpp
@@ -0,0 +1,4 @@
+// Inciso b:Valentin Calcular el factorial del número entero
+    int numero_entero = static_cast<int>(numero); // Convertir el número a entero
+    unsigned long long fact = factorial(numero_entero);
+    std::cout << "b) El factorial de " << numero_entero << " es: " << fact << std::endl;
